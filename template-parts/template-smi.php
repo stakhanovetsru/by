8a3092d4e5722b8c6c00edc0839bebfac66a1d86
WP Admin <dev@omsk.dev>
2024-08-21 13:33:58 +0300

    add thumbnail CPT reviews

--- template-parts/template-smi.php
+++ template-parts/template-smi.php
@@ -3,7 +3,7 @@
         <h2 class="euro-medium h2 text-center mb-5">СМИ о нас</h2>
         <div class="dlp__smi_posts row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 row-gap-4">
 			<?php
-			if ( false === $posts = get_transient( 'stkh_smi_image_cache' ) ) {
+			if ( false === $posts = get_transient( 'stkh_smi_cache' ) ) {
 				$response = wp_remote_get(
 					add_query_arg(
 						array(
@@ -18,8 +18,7 @@
 
 				if ( 200 === wp_remote_retrieve_response_code( $response ) ) {
 					$posts = json_decode( wp_remote_retrieve_body( $response ), true );
-					set_transient( 'stkh_smi_image_cache', $posts, HOUR_IN_SECONDS );
-					// stkh_smi_remote_cache
+					set_transient( 'stkh_smi_cache', $posts, HOUR_IN_SECONDS );
 				}
 			}
 
@@ -31,6 +30,10 @@
                         <a target="_blank" href="<?php echo esc_url( $post['link'] ); ?>" class="dlp__smi_post"
                            rel="nofollow noopener noreferrer">
                             <div class="dlp__smi_box">
+                                <div class="dlp__smi_image">
+                                    <img src="<?php echo 'https://stakhanovets.ru' . $post['carbon_image_logo'] ?>"
+                                         alt="<?php echo esc_attr( $post['title']['rendered'] ); ?>" class="img-fluid"/>
+                                </div>
                                 <div class="dlp__site_desc">
                                     <img src="<?php echo 'https://stakhanovets.ru' . $post['featured_image_src'] ?>"
                                          alt="<?php echo esc_attr( $post['title']['rendered'] ); ?>"/>
@@ -42,7 +45,7 @@
                                 </div>
                             </div>
                             <div class="dlp__smi_title">
-								<?php echo wp_trim_words( $post['title']['rendered'], 8 ); ?>
+								<?php echo wp_trim_words( $post['title']['rendered'], 9 ); ?>
                             </div>
                         </a>
 
